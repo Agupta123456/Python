710e6c8267c303ce968b04c1ba07139de5c59a51
Ajit Gupta <91139365+Agupta123456@users.noreply.github.com>
2023-09-25 17:51:50 +0530

    Add files via upload

---
+++ Python/string1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 22 16:40:17 2023
+
+@author: ajit2
+"""
+
+name="Ajit"
+salary=19000
+print("Ara Ara senpai {0} your acc credited by {1}". format(name,salary))
