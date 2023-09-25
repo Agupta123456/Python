2f94f1cfad317ecd99bfc9230b76f6670e63d052
Ajit Gupta <91139365+Agupta123456@users.noreply.github.com>
2023-09-25 17:56:34 +0530

    Delete Python directory

--- Python/operator4.py
+++
@@ -1,4 +0,0 @@
-x = 5
-print(x > 3 and x < 10)
-print(x > 3 or x < 4)
-print(not(x > 3 and x < 10))
--- Python/operator5.py
+++
@@ -1,9 +0,0 @@
-a=2
-b=4
-print(a&b)
-print(a|b)
-print(~a)
-print(~b)
-print(a<<b)
-print(a>>b)
-print(a^b)
